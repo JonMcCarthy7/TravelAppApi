aaa8b5b68017d64e02267f487cb8406c49694b4d
Jon McCarthy <jtmcc93@gmail.com>
2018-11-28 10:02:20 -0700

    fix CORS problem, added middleware to application.rb

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@ gem 'puma', '~> 3.11'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-gem 'rack-cors'
+gem 'rack-cors', :require => 'rack/cors'
 gem 'active_model_serializers'
 
 # Reduces boot times through caching; required in config/boot.rb
--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -2,9 +2,40 @@
 
 module Api::V1
   class TripsController < ApiController
+    before_action :set_trip, only: %i[show update destroy]
+    
     def index
-      trips = Trip.all
-      render json: { status: 'SUCCESS', message: 'Loaded all trips', data: trips }, status: :ok
+      @trips = Trip.all
+      render json: { status: 'SUCCESS', message: 'Loaded all trips', data: @trips }, status: :ok
+    end
+
+    def create
+      @trip = Trip.create!(trip_params)
+      render json: { status: 'SUCCESS', message: 'Trip Created', data: @trip }, status: :created
+    end
+
+    def show
+      render json: { status: 'SUCCESS', message: 'Loaded Trip', data: @trip }, status: :ok
+    end
+
+    def update
+      @trip.update(trip_params)
+      head :no_content
+    end
+
+    def destroy
+      @trip.destroy
+      head :no_content
+    end
+
+    private
+
+    def trip_params
+      params.permit(:user_id, :name, :start_date, :end_date, :country, :description, :created_at, :updated_at)
+    end
+
+    def set_trip
+      @trip = Trip.find(params[:id])
     end
   end
 end
